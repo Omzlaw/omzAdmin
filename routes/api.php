97dee8b1be2520c0387e4a39d1cebd24f573dcdc
Omeiza <omeiza.alabi@edsinetechnologiesltd.com>
2021-09-13 15:54:31 +0100

    first commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+
+
+
+
+
+Route::resource('activity_logs', App\Http\Controllers\API\ActivityLogAPIController::class);
+
+
