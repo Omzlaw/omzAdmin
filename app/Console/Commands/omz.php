f8a38b2405ca6d85f4b894db3e84406f5890e8ab
Omeiza <omeiza.alabi@edsinetechnologiesltd.com>
2021-09-29 01:00:38 +0100

    add custom artisan command

---
+++ app/Console/Commands/omz.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Composer;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class omz extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'omz';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'First time setup for Omz Admin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        app()->make(Composer::class)->run(['install']);
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
+    }
+}
